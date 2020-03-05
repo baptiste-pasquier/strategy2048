afd2b121816b79a6b96b8cc689fd847480070c76
Baptiste Pasquier <50556298+baptiste-pasquier@users.noreply.github.com>
2020-03-05 01:04:36 +0100

    Add files via upload

--- example.py
+++ example.py
@@ -1,44 +1,67 @@
 import numpy
 import matplotlib.pyplot as plt
 import time
-
 from strategy2048 import Game2048, random_strategy, evaluate_strategy, nextmovescorebest
+
+
+nb_test = 20
 
 
 ##############################
 # Stratégie aléatoire
 
-
 temps1 = time.time()
-gen1 = evaluate_strategy(random_strategy, 100)
+gen1 = evaluate_strategy(random_strategy, nb_test)
 res1 = list(gen1)
 res1.sort()
 print("Stratégie aléatoire : " + str(res1))
 print("Durée : " + str((time.time() - temps1)))
-print("proportion >= 2048 =", ((numpy.array(res1) >= 2048).sum() / len((res1))))
+print("Moyenne =", numpy.mean(res1))
+print("Proportion >= 2048 =", ((numpy.array(res1) >= 2048).sum() / len((res1))))
 print()
+
 
 ##############################
 # Notre stratégie
 
 temps2 = time.time()
-gen2 = evaluate_strategy(nextmovescorebest, 100)
+gen2 = evaluate_strategy(nextmovescorebest, nb_test)
 res2 = list(gen2)
 res2.sort()
-print("Notre stratégie : " + str(res2))
+print("Mystrategy : " + str(res2))
 print("Durée : " + str((time.time() - temps2)))
-print("proportion >= 2048 =", ((numpy.array(res2) >= 2048).sum() / len((res2))))
+print("Moyenne =", numpy.mean(res2))
+print("Proportion >= 2048 =", ((numpy.array(res2) >= 2048).sum() / len((res2))))
 
 
 #########################################
-# Finaly plots the gains obtained by the two strategies.
+# Graphiques résultats
 
-fig, ax = plt.subplots(1, 1, figsize=(8, 4))
-ax.bar(numpy.arange(len(res1)), res1, color="b",
-       label="aléatoire", width=0.4)
-ax.bar(numpy.arange(len(res2)) + 0.4, res2, color="orange",
-       label="notre stratégie", width=0.4)
-ax.set_title("Compares two strategies for 2048.")
-ax.legend()
+# fig, ax = plt.subplots(1, 1, figsize=(8, 4))
+# ax.bar(numpy.arange(len(res1)), res1, color="b",
+#        label="Stratégie aléatoire", width=0.4)
+# ax.bar(numpy.arange(len(res2)) + 0.4, res2, color="orange",
+#        label="mystrategy", width=0.4)
+# ax.set_title("Compares two strategies for 2048.")
+# ax.legend()
+# plt.show()
 
-plt.show()
+
+def affiche_distribution(liste, nom):
+    liste = sorted(liste)
+    liste = numpy.array(liste)
+    D = {}
+    for elem in liste:
+        if elem in D:
+            D[elem] += 1
+        else:
+            D[elem] = 1
+    plt.bar(range(len(D)), list(D.values()), align='center')
+    plt.xticks(range(len(D)), list(D.keys()))
+    plt.xlabel("Score final")
+    plt.title(nom)
+    plt.show()
+
+
+affiche_distribution(res1, "Distribution stratégie aléatoire")
+affiche_distribution(res2, "Distribution mystrategy")
